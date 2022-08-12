7f0ea4b65d7e1ad6394c1d9eca96ae6f78a366b3
Jheison J. Mayta Cáceres <jmayta.caceres@gmail.com>
2022-08-12 14:50:52 -0500

    ⚙️ conf: add webpack + babel conf

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+// ------------------------------------------------- Global imports & variables
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require("path");
+
+// ---------------------------------------------------------------------- Rules
+// Style
+const ruleForStyles = [
+  {
+    test: /\.css|\.s[ca]ss$/i,
+    use: [MiniCSSExtractPlugin.loader, "css-loader", "sass-loader"],
+  },
+];
+
+// JavaScript
+const ruleForJavaScript = [
+  {
+    test: /\.m?js$/i,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+    },
+  },
+];
+
+// ----------------------------------------------------------------------- Main
+
+// Rules loader
+const rules = [].concat(ruleForStyles, ruleForJavaScript);
+
+// Configuration
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  const isProduction = mode === "production";
+
+  return {
+    entry: {
+      index: path.resolve(__dirname, "src", "index.js"),
+    },
+    module: {
+      rules,
+    },
+    output: {
+      filename: isProduction ? "main.[contenthash].js" : "main.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
+      new MiniCSSExtractPlugin({
+        filename: isProduction ? "style.[contenthash].css" : "style.css",
+      }),
+    ],
+  };
+};
